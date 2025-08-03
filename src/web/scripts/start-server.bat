dc785bbd7d0465dd15e450a4327688da90d89322
waynechen251 <waynechen251@gmail.com>
2025-08-03 23:39:00 +0800

    steamcmd script

--- src/web/scripts/start-server.bat
+++ src/web/scripts/start-server.bat
@@ -1,9 +1,11 @@
 @echo off
 REM param: %1 - 7DTD server path
+REM param: %2 - CONFIG_FILE
 
 set "SERVER_DIR=%~1"
+set "CONFIG_FILE=%~2"
 
-pushd "%SERVER_DIR%"
-if not exist "Logs" mkdir Logs
-for /f "tokens=* skip=20" %%f in ('dir Logs\output_log_*.txt /b /o-d') do del "Logs\%%f"
-popd
+for /f %%i in ("%date:~0,10%_%time:~0,8%") do set TS=%%i
+set TS=%TS::=_% & set TS=%TS:/=-% & set TS=%TS: =0%
+
+start "" "%SERVER_DIR%\7DaysToDieServer.exe" -logfile "%SERVER_DIR%\Logs\output_log_%TS%.txt" -quit -batchmode -nographics "-configfile=%CONFIG_FILE%" -dedicated
